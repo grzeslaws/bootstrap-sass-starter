16bff3dc778042e4fc51e8b894b5830af1f356a9
grzeslaws <grzesupel@gmail.com>
2017-04-12 17:40:23 +0200

    poprawka gulpa

--- gulpfile.js
+++ gulpfile.js
@@ -14,28 +14,3 @@ gulp.task('sass', function () {
 gulp.task('default', function () {
   gulp.watch('./scss/*.scss', ['sass']);
 });
-
-
-
-// var gulp = require('gulp');
-// var less = require('gulp-sass');
-//
-//
-// var sassPaths = [
-//   '*.scss'
-// ];
-//
-//
-// gulp.task('sass', function () {
-//   return gulp.src('scss/app.scss')
-//     .pipe(scss({
-//       paths: sassPaths,
-//       outputStyle: 'compressed'
-//     }))
-//     .pipe(gulp.dest('/css'));
-// });
-//
-//
-//     gulp.task('default', ['sass'], function() {
-//       gulp.watch(['*.sass', '*.sass'], ['sass']);
-// });
